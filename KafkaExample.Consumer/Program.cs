6ce988806d35b5974b0c272d86e84705de3455b0
sliusarenko.denys@gmail.com <sliusarenko.denys@gmail.com>
2026-02-12 13:56:54 +0200

    Add project files.

---
+++ KafkaExample.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Confluent.Kafka;
+
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "test-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe("test-topic");
+
+Console.WriteLine("Listening for messages...");
+
+try
+{
+    while (true)
+    {
+        var cr = consumer.Consume();
+        Console.WriteLine($"Received: {cr.Message.Value}");
+    }
+}
+catch (OperationCanceledException)
+{
+    consumer.Close();
+}
---
+++ KafkaExample.Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    var result = await producer.ProduceAsync(
+        "test-topic",
+        new Message<Null, string> { Value = "Message from producer" });
+
+    Console.WriteLine($"Delivered to: {result.TopicPartitionOffset}");
+}
+catch (ProduceException<Null, string> e)
+{
+    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+}
